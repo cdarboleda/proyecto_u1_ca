8768c81cec414f92cf027f4781b23f59b184c0f2
Cristian Arboleda <cdarboleda@uce.edu.ec>
2022-06-17 20:10:21 -0500

    Lookup en Spring Framework

--- src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
+++ src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.uce.edu.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Service;
 
 import com.uce.edu.demo.modelo.Matricula;
@@ -18,11 +19,27 @@ public class MatriculaServiceImpl implements IMatriculaService{
 	@Autowired
 	private ProfesorMateria profesorMateria;
 	
+	@Lookup
+	public ProfesorMateria obtenerProfesorM() {
+		ProfesorMateria profeM = new ProfesorMateria();
+		profeM.setApellido("Tapia");
+		profeM.setNombre("Jose");
+		return profeM;
+	}
+	
+	@Lookup
+	public ProfesorGeneral obtenerProfesorG() {
+		return null;
+	}
+	
 	@Override
 	public void crearMatricula(Matricula m) {
 		// TODO Auto-generated method stub
 		System.out.println("DI desde Service SINGLETON " + this.profesorGeneral);
 		System.out.println("DI desde Service PROTOTYPE " + this.profesorMateria);
+		
+		System.out.println("DI desde un metodo profesor general: " + this.obtenerProfesorG());
+		System.out.println("DI desde un metodo profesor materia: " + this.obtenerProfesorM());
 		this.matricula.insertar(m);
 	}
 
